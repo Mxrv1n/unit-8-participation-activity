971238a988bb5d1613bb2bdab854764b0789f3b1
Marvin P <MarvinPerez1503@outlook.com>
2026-03-06 02:13:51 -0500

    8-4 complete

--- tshirt.py
+++ tshirt.py
@@ -6,8 +6,16 @@ on the shirt. The functio  should print a sentence summarizing the size of the s
 
 Call the function once using positional arguments to make a shirt. Call the function a second time using keyword arguments.
 """
-def make_shirt(size, message):
+def make_shirt(size='l', message='I love python'):
     print(f"A shirt of size {size.upper()} will have the following message printed on it:\n{message}")
-make_shirt("xl",'hang in there')
-make_shirt('l','need a coffee')
 
+make_shirt('l')
+make_shirt('m')
+make_shirt('s','Programming is fun')
+
+"""
+8-4 
+Large shirts
+Modify the make_shirt() function so that shirts are large by default with a message that reads "I love python".
+Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+"""
